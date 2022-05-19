917b2f2d6733ab01a7af9174ac1441633042f31b
Wilson <wilsontancher@gmail.com>
2022-05-20 03:24:20 +0800

    Add in Lab5
    Add  package-lock.json into .gitignore.
    Add in Lab5 solution

---
+++ lab5/Lab5Task2/calculator.js
@@ -0,0 +1,18 @@
+console.log("Starting calculator app");
+//console.log(module);
+
+module.exports = {
+    description : "This is my calculator",
+    add : function(a,b) {
+        return a+b;
+    },
+    multiply: function(a,b) {
+        return a*b;
+    },
+    subtract: function(a,b)  {
+        return a-b;
+    },
+    divide: function(a,b) {
+        return a/b;
+    }
+};
